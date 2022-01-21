28d0e2fe6cbdb65149c774e204f63319491da675
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2022-01-21 12:02:11 -0300

    Initial Commit

---
+++ TikTokLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokLayout
+//
+//  Created by Vanesa Korbenfeld on 21/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
